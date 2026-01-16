1f100b04cd06647efe6a4667364ad939d690a333
Lukasz Dudzinski <ldudzinski@sii.pl>
2026-01-16 22:39:00 +0100

    ContentView instead of IsVisible

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Windows.Input;
 using MauiStart.Base;
 using MauiStart.Models;
 
@@ -6,30 +7,51 @@ namespace MauiStart.ViewModels;
 
 public class MainViewModel : BaseViewModel
 {
-    public override bool CanNavigateBack => false;
+    public ICommand SelectItemCommand { get; }
 
-    // Page provider used by the InfiniteCollectionViewComponent (adapt to non-generic IList)
-    public Func<int, int, Task<IList>> LoadPage => async (page, pageSize) => (IList)await GetUsersAsync(page, pageSize);
+    private string _selectedIndex;
+
+    public string SelectedIndex
+    {
+        get => _selectedIndex;
+        set => SetProperty(ref _selectedIndex, value);
+    }
+    
+    // public override bool CanNavigateBack => false;
+    //
+    // // Page provider used by the InfiniteCollectionViewComponent (adapt to non-generic IList)
+    // public Func<int, int, Task<IList>> LoadPage => async (page, pageSize) => (IList)await GetUsersAsync(page, pageSize);
+    
+    public MainViewModel()
+    {
+        SelectItemCommand = new Command<string>((arg) => SelectBox(arg));
+
+    }
 
     public override async Task OnViewAppearing()
     {
         await base.OnViewAppearing();
     }
 
-    // Simulated paged data source
-    private static async Task<IList<User>> GetUsersAsync(int page, int pageSize)
+    private void SelectBox(string boxNumber)
     {
-        await Task.Delay(1500); // simulate network latency
-
-        var start = (page - 1) * pageSize;
-        var total = 100; // pretend there's a total number of users
-
-        if (start >= total)
-            return Array.Empty<User>();
-
-        var count = Math.Min(pageSize, total - start);
-        return Enumerable.Range(start, count)
-            .Select(i => new User($"User {i + 1}", $"user{i + 1}@example.com"))
-            .ToList();
+        SelectedIndex = boxNumber;
     }
+
+    // Simulated paged data source
+    // private static async Task<IList<User>> GetUsersAsync(int page, int pageSize)
+    // {
+    //     await Task.Delay(1500); // simulate network latency
+    //
+    //     var start = (page - 1) * pageSize;
+    //     var total = 100; // pretend there's a total number of users
+    //
+    //     if (start >= total)
+    //         return Array.Empty<User>();
+    //
+    //     var count = Math.Min(pageSize, total - start);
+    //     return Enumerable.Range(start, count)
+    //         .Select(i => new User($"User {i + 1}", $"user{i + 1}@example.com"))
+    //         .ToList();
+    // }
 }
